2630bbac1260f16b0c76b7d0bc0f05672cfc13c3
dallis <serhiy.vm@gmail.com>
2011-06-22 15:09:04 -0700

    design

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,14 @@
 class Vacancy < ActiveRecord::Base
 	belongs_to :category
 	has_many :contacts
+	
+	def previous_vacancy
+  		self.class.first(:conditions => ["id < ?", id], :order => "id desc")
+	end
+	
+	def next_vacancy
+  		self.class.first(:conditions => ["id > ?", id], :order => "id asc")
+	end
+
+
 end
