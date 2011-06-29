913ed937650111a91b2b8c58d5399cfdcfc6efd3
Ubuntums <serhiy@ubuntu.localdomain>
2011-06-29 12:14:01 -0700

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 WestPartners::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/faq"
+
+  get "pages/articles"
+
   resources :vacancies
 
   # The priority is based upon order of creation:
