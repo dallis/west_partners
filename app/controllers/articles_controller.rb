c581cd755a6087c51de23b0f8244092b7ade49d2
Ubuntums <serhiy@ubuntu.localdomain>
2011-07-23 04:07:58 -0700

    add art

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -67,7 +67,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to(articles_url) }
+      format.html { redirect_to(root_path) }
       format.xml  { head :ok }
     end
   end
